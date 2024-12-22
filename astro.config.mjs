f975fc3cd432864f0e971a038ce3b966e61f8a49
Simon Mei <smei8800@gmail.com>
2024-12-21 23:02:07 -0500

    Add Greeting component to Homepage
    Add dark mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://starlit-axolotl-869091.netlify.app/",
+  integrations: [preact()],
 });
